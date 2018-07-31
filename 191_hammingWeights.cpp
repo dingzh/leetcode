35f2e87bd40225ac267dfc042a87db74ee4f8863
Ding Zhou <zhouding@sjtu.edu.cn>
2018-07-31 17:43:18 +0800

    5 steps

--- 191_hammingWeights.cpp
+++ 191_hammingWeights.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int ret = 0;
-        while (n) {
-            ++ret;
-            n = n & (n - 1);
-        }
+        n = (0x55555555 & n) + (0x55555555 & (n>>1));
+        n = (0x33333333 & n) + (0x33333333 & (n>>2));
+        n = (0x0F0F0F0F & n) + (0x0F0F0F0F & (n>>4));
+        n = (0x00FF00FF & n) + (0x00FF00FF & (n>>8));
+        n = (0x0000FFFF & n) + (0x0000FFFF & (n>>16));
         
-        return ret;
+        return n;
     }
 };
