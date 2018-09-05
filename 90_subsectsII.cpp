62190384f32d33409c2b852324ae6e10c88cffb2
Ding Zhou <zhouding@sjtu.edu.cn>
2018-09-05 00:54:57 -0400

    using ordered_map, sacrifice space for time

--- 90_subsectsII.cpp
+++ 90_subsectsII.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        map<int, int> numsCount;
+        unordered_map<int, int> numsCount;
 
         for (auto n : nums) {
-            auto lb = numsCount.lower_bound(n);
-            if (lb != numsCount.end() && lb->first == n) {
-                lb->second += 1;
+            auto res = numsCount.find(n);
+            if (res != numsCount.end()) {
+                res->second += 1;
             } else {
-                numsCount.insert(lb, {n, 1});
+                numsCount.insert({n, 1});
             }
         }
 
@@ -16,11 +16,11 @@ public:
         vector<int> subset;
 
         helper(numsCount.cbegin(), numsCount, subset, ret);
-        
+
         return ret;
     }
 
-    void helper(map<int,int>::const_iterator cit, map<int,int>& numsCount, vector<int>& subset, vector<vector<int>>& ret) {
+    void helper(unordered_map<int,int>::const_iterator cit, unordered_map<int,int>& numsCount, vector<int>& subset, vector<vector<int>>& ret) {
         if (cit == numsCount.cend()) {ret.push_back(subset); return;}
 
         int num = cit->first;
