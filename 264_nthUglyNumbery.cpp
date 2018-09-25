9f339d199cc0464749846b5b0af2efd1e839e231
Ding Zhou <zhouding@sjtu.edu.cn>
2018-09-25 18:46:23 -0400

    constrain candidates

--- 264_nthUglyNumbery.cpp
+++ 264_nthUglyNumbery.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if (n == 1) return 1;
+        if (n < 2) return n;
         
-        --n;
-        priority_queue<int, vector<int>, greater<int>> pq = {2,3,5};
-        
-        while (--n) {
-            int top = pq.top();
-            while (top == pq.top()) {
-                pq.pop();
-            }
+        int* ugly_num = new int[n + 1];
+        ugly_num[1] = 1;
 
-            pq.push(top * 2);
-            pq.push(top * 3);
-            pq.push(top * 5);
+        int t2 = 1, t3 = 1, t5 = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int n2 = ugly_num[t2] * 2;
+            int n3 = ugly_num[t3] * 3;
+            int n5 = ugly_num[t5] * 5;
+            int next = min(n2, min(n3, n5));
+            
+            if (n5 == next) ++t5;
+            if (n3 == next) ++t3;
+            if (n2 == next) ++t2;
+
+            ugly_num[i] = next;
         }
 
-        return pq.top();
+        return ugly_num[n];
     }
 };
