8d25713d6afcdd8bce056285c805484bc11d70ac
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-04 02:11:44 -0400

    invariant rt on the lower half

--- 153_findMinRotated.cpp
+++ 153_findMinRotated.cpp
@@ -3,14 +3,13 @@ public:
     int findMin(vector<int>& nums) {
         int size = nums.size();
         if (!size) return 0;
-        if (size == 1 ) return nums[0];
 
         int lt = 0, rt = size - 1;
-        while (lt != rt) {
+        while (lt < rt) {
             int mid = (lt + rt) >> 1;
-            if (nums[mid] > nums[lt]) lt = mid;
-            else rt = mid;
+            if (nums[mid] < nums[rt]) rt = mid;
+            else lt = mid + 1;
         }
-        return min(nums[lt + 1], nums[0]); // in case monotonically increasing
+        return nums[rt];
     }
 };
