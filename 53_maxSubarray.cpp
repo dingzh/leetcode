ca808869a83a0c4327c9d622cdb9afe2ad8a30a3
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-07 01:31:32 -0400

    clean O(n)

--- 53_maxSubarray.cpp
+++ 53_maxSubarray.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        return helper(nums, 0, nums.size()-1);
-    }
-
-    int helper(vector<int>& nums, int lt, int rt) {
-        if (lt == rt) return nums[lt];
-        int mid = (lt + rt) >> 1;
-
-        int ltMax = helper(nums, lt,mid);
-        int rtMax = helper(nums, mid+1, rt);
-
-        int sum = 0;
-        int midLeftMax = 1u << 31, midRightMax = 1u << 31;
-        for (int i = mid; i >= lt; --i) {
-            sum += nums[i];
-            midLeftMax = max(midLeftMax, sum);
+        int ret = nums[0], max_suffix = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            max_suffix = max(max_suffix, 0) + nums[i];
+            ret = max( ret, max_suffix);
         }
-
-        sum = 0;
-        for (int i = mid+1; i <= rt; ++i) {
-            sum += nums[i];
-            midRightMax = max(midRightMax, sum);
-        }
-
-        return max(max(ltMax, rtMax), midLeftMax+midRightMax);
+        return ret;
     }
 };
