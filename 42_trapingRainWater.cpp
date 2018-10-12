75ad543c70102c5d1b60b949b4ede916540a70d7
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-11 20:39:13 -0400

    1 pass

--- 42_trapingRainWater.cpp
+++ 42_trapingRainWater.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int size = height.size();
-        vector<int> lheight(size, 0);
-        
-        int max_height = 0;
-        for (int i = 0; i < size; ++i) {
-            lheight[i] = max_height;
-            max_height = max(max_height, height[i]);
-        }
+        int ret = 0, lt = 0, rt = size -1;
+        int lmax = 0, rmax = 0;
 
-        max_height = 0;
-        int ret = 0;
-        for (int i = size-1; i > -1; --i) {
-            int water = min(lheight[i], max_height) - height[i];
-            max_height = max(max_height, height[i]);
-            ret += max(0, water);
+        while (lt <= rt) {
+            if (lmax <= rmax) {
+                ret += max(0, lmax - height[lt]);
+                lmax = max(height[lt], lmax);
+                ++lt;
+            } else {
+                ret += max(0, rmax - height[rt]);
+                rmax = max(height[rt], rmax);
+                --rt;
+            }
         }
-       
         return ret;
     }
 };
