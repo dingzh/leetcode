62e195b069c44b9e01b2f5703c656218ac16dff8
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-18 00:23:57 -0400

    clean code with stl

--- 31_nextPerm.cpp
+++ 31_nextPerm.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int size = nums.size();
-        if (size <= 1) return;
-
-        int i = size - 2;
-        while ( i > -1 && nums[i] >= nums[i+1]) --i;
-        if (i == -1) {
-            reverse( nums, 0, size - 1 );
-            return;
+        auto inversion_point = is_sorted_until(nums.rbegin(), nums.rend());
+        
+        if (inversion_point == nums.rend()) {
+            reverse(nums.begin(), nums.end());
+            return ;
         }
-
-        int p = i + 1;
-        int k = p;
-        while (k < size && nums[k] > nums[i]) ++k;
-        --k;
-
-        swap(nums, i, k);
-        reverse(nums, p, size - 1);
-    }
-    
-    // end included
-    void reverse(vector<int> &nums, int begin, int end) {
-        while (begin < end) {
-            swap(nums, begin, end);
-            ++begin;
-            --end;
-        }
-    }
-
-    void swap(vector<int> &nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
+        
+        auto least_upper_bound =
+            upper_bound(nums.rbegin(), inversion_point, *inversion_point);
+        
+        iter_swap(least_upper_bound, inversion_point);
+        reverse(nums.rbegin(), inversion_point);
     }
 };
