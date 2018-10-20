81046424de4351841a68969daacdfc8fcdd226f0
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-20 00:11:06 -0400

    cleaner code

--- 21_mergeTwoLists.cpp
+++ 21_mergeTwoLists.cpp
@@ -9,22 +9,19 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-  	ListNode head {0};
-	ListNode* curr = &head;
+        ListNode dummy {0};
+        ListNode* tail = &dummy;
 
-	while (l1!= nullptr && l2 != nullptr) {
-		if (l1->val <= l2->val) {
-			curr->next = l1;
-			l1 = l1->next;
-		} else {
-			curr->next = l2;
-			l2 = l2->next;
-		}
-		curr = curr->next;
-	}
+        while (l1 && l2) {
+            appendNode(l1->val <= l2->val ? l1 : l2, tail);
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
 
-	curr->next = l1 ? l1 : l2;
-
-	return head.next;
+    void appendNode(ListNode* &node, ListNode* &tail) {
+        tail->next = node;
+        tail = node;
+        node = node->next;
     }
 };
