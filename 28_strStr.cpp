0ec57c1cc30f1e0a2c0ba7a918669a9311883005
Ding Zhou <zhouding@sjtu.edu.cn>
2018-10-30 23:48:12 -0400

    Robin Carp

--- 28_strStr.cpp
+++ 28_strStr.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (needle.length() == 0) return 0;
-        int shift[128];
-        for (int i = 0; i < 128; ++i) shift[i] = -1;
 
-        for (int i = 0; i < needle.length(); ++i) {
-                shift[needle[i]] = i;
+        //using prime = 2^32, which save us from using modulo
+        unsigned int kbase = 257; // which is a prime
+
+        int n_len = needle.length(), h_len = haystack.length();
+        if (h_len < n_len) return -1;
+
+        unsigned int n_hash = 0, h_hash = 0;
+        for (int i = 0; i < n_len; ++i) {
+            // may overflow, but wont be an issue
+            n_hash = n_hash * kbase + needle[i];
+            h_hash = h_hash * kbase + haystack[i];
+        }
+        // cout << n_hash << " " << h_hash << endl;
+
+        unsigned int power = 1;
+        for (int i = 1; i < n_len; ++i) power *= kbase;
+
+        for (int i = n_len; i < h_len; ++i) {
+            if (n_hash == h_hash) {
+                // validate
+                return i-n_len;
+            }
+
+            h_hash -= power * haystack[i-n_len];
+            h_hash = h_hash * kbase + haystack[i];
         }
 
-        int i = 0;
-        int sLen = haystack.length(), pLen = needle.length();
-
-        while (i + pLen <= sLen) {
-                int j = 0;
-                while (j < pLen && haystack[i+j] == needle[j]) ++j;
-                if (j == pLen) {
-                        return i;
-                } else if(i + pLen < sLen) {
-                        i = i + pLen - shift[haystack[i+pLen]];
-                } else {
-                        return -1;
-                }
-        }
-
-        return -1;
+        return h_hash == n_hash ? h_len - n_len : -1;
+		// TODO if needed, verify to avoid collision
     }
 };
