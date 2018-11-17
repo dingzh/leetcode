d798870b99e1104ec0e523b5b3d2a4777a7d6af0
Ding Zhou <zhouding@sjtu.edu.cn>
2018-11-17 18:01:59 -0500

    counting ones at same pos with bitset

--- 750_numCornerRectangle.cpp
+++ 750_numCornerRectangle.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     int countCornerRectangles(vector<vector<int>>& grid) {
-        unordered_map<int, int> corner_count;
-        
         int row = grid.size();
         if (row < 2) return 0;
         int col = grid[0].size();
         if (col < 2) return 0;
-        
+
         int ret = 0;
+        vector<bitset<200>> ones(row, bitset<200>());
         for (int r = 0; r < row; ++r) {
-            vector<int> ones;
-            for (int i = 0; i < col; ++i) {
-                if (grid[r][i]) ones.push_back(i);
-            }
-            
-            vector<int> corner_pairs = get_pairs(ones);
-            for (auto& cp : corner_pairs) {
-                ret += corner_count[cp]++;
+            for (int c = 0; c < col; ++c) {
+                if (grid[r][c]) ones[r].set(c);
             }
         }
-        
-        return ret;
-    }
-    
-    vector<int> get_pairs(vector<int>& ones) {
-        int size = ones.size();
-        vector<int> ret;
-        for (int i = 0; i < size; ++i) {
-            for (int j = i + 1; j < size; ++j) {
-                ret.push_back(ones[i]* 201 + ones[j]);
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = i + 1; j < row; ++j) {
+                int cnt = (ones[i] & ones[j]).count();
+                ret += (cnt * (cnt - 1)) / 2;
             }
         }
+
         return ret;
     }
+
 };
