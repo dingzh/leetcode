e6177373b32d3b419453c1f72fafd0246b0f9d12
Ding Zhou <zhouding@sjtu.edu.cn>
2018-11-26 19:46:24 -0500

    ordering using priority queue instead of set

--- 846_handsOfStraights.cpp
+++ 846_handsOfStraights.cpp
@@ -3,27 +3,24 @@ public:
     bool isNStraightHand(vector<int>& hand, int W) {
         int size = hand.size();
         
-        set<int> hand_types;
+        priority_queue<int, vector<int>, greater<int>> hand_types;  // for ordering
         unordered_map<int, int> hand_cnts;
         
         for (int h : hand) {
             if (++hand_cnts[h] == 1) 
-                hand_types.insert(h);
+                hand_types.push(h);
         }
 
-        while (hand_types.size()) {
-            int min_hand = *hand_types.begin();
+        while (!hand_types.empty()) {
+            int min_hand = hand_types.top();
+            hand_types.pop();
+            
             int hand_num = hand_cnts[min_hand];
+            if (!hand_num) continue ;
             for (int i = 0; i < W; ++i) {
-                int this_num = hand_cnts[min_hand + i];
-                if (this_num < hand_num) {
-                    return false;
-                } else if (this_num == hand_num) {
-                    hand_cnts.erase(min_hand+i);
-                    hand_types.erase(min_hand+i);
-                } else  {
-                    hand_cnts[min_hand + i] -= hand_num;
-                }
+                int& this_num = hand_cnts[min_hand + i];
+                this_num -= hand_num;
+                if (this_num < 0) return false;
             }
         }
 
