e8663a7ff2942dbb2d4b121e88f72bb022028469
Zhou Ding <dz275@zoo.cs.yale.edu>
2018-12-22 23:12:44 -0500

    optimized space for DP

--- 727_minWindowSubseq.cpp
+++ 727_minWindowSubseq.cpp
@@ -4,18 +4,20 @@ public:
         int slen = S.size(), tlen = T.size();
         if (slen < tlen) return "";
 
-        vector<vector<int>> dp(slen, vector<int>(tlen, -1));
-        dp[0][0] = S[0] == T[0] ? 0 : -1;
+        vector<int> dp(tlen, -1);
+        dp[0] = S[0] == T[0] ? 0 : -1;
 
         int min_len = slen + 1;
         int min_start = 0;
         for (int i = 1; i < slen; ++i) {
-            dp[i][0] = S[i] == T[0] ? i : dp[i-1][0];
 
-            for (int j = 1; j < tlen; ++j) {
-                dp[i][j] = S[i] == T[j] ? dp[i-1][j-1] : dp[i-1][j];
+            for (int j = tlen - 1; j > 0; --j) {
+                if(S[i] == T[j])  dp[j] = dp[j-1];
             }
-            int start = dp[i].back();
+
+            if (S[i] == T[0]) dp[0] = i;
+
+            int start = dp.back();
             if (start != -1 && i - start + 1 < min_len) {
                 min_start = start;
                 min_len = i - start + 1;
